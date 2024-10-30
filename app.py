e66e295a6ebfbcd1941caa42552c74c5a25cddc0
Yoselin <yoselin.morva@gmail.com>
2024-10-30 00:50:03 -0500

    Agregue una tabla de datos y una casilla de verificacion
    La tabla muestra todos los datos de los vehiculos.
    La casilla de verificacion nos muetra los autos mas usados

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-        
+
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+
+
+st.markdown("<h1 style='color: black;'>Análisis de Datos de Vehículos</h1>", unsafe_allow_html=True) #Titulo, color y tamano
+
+
+st.markdown("<h2 style='color: red;'>Visión General de los Datos</h2>", unsafe_allow_html=True)
+
+if st.checkbox('Mostrar modelos más usados'):
+    st.subheader("Modelos más usados")
+    model_counts = car_data['model'].value_counts()
+    st.write(model_counts.head(15))  # Muestra los 15 modelos más comunes
+
+st.dataframe(car_data)#Tabla de datos generales
+
+
 hist_button = st.button('Construir histograma') # crear un botón
 
 if hist_button: # al hacer clic en el botón # escribir un mensaje
@@ -11,4 +26,4 @@ if hist_button: # al hacer clic en el botón # escribir un mensaje
     fig = px.histogram(car_data, x="odometer")
     # mostrar un gráfico Plotly interactivo 
     st.plotly_chart(fig, use_container_width=True)
-    
+
