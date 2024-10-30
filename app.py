4cfd11c12f484a2e54c19727f4cb4bf8f07f9f6e
Yoselin <yoselin.morva@gmail.com>
2024-10-30 02:21:19 -0500

    Agregue grafica e histogramas
    El histograma que muestra datos de anuncios de venta se modifico el color.
    Se agrego un nuevo histograma que nos muestra la relación entre la condición del vehículo y el año del modelo.
    Se agrego una grafica de dispersión que muestra el precio vs la condición del vehiculo.

--- app.py
+++ app.py
@@ -10,20 +10,56 @@ st.markdown("<h1 style='color: black;'>Análisis de Datos de Vehículos</h1>", u
 
 st.markdown("<h2 style='color: red;'>Visión General de los Datos</h2>", unsafe_allow_html=True)
 
-if st.checkbox('Mostrar modelos más usados'):
-    st.subheader("Modelos más usados")
+if st.checkbox('Mostrar modelos más vendidos'):
+    st.subheader("Modelos más vendidos")
     model_counts = car_data['model'].value_counts()
     st.write(model_counts.head(15))  # Muestra los 15 modelos más comunes
 
 st.dataframe(car_data)#Tabla de datos generales
 
 
-hist_button = st.button('Construir histograma') # crear un botón
+hist_button = st.button('Datos de anuncios de venta') # crear un botón
 
 if hist_button: # al hacer clic en el botón # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Histograma que muestra el conjunto de datos de anuncios de venta de coches')
     # crear un histograma 
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x="odometer", color_discrete_sequence=["orange"])
     # mostrar un gráfico Plotly interactivo 
     st.plotly_chart(fig, use_container_width=True)
 
+
+# Botón para construir un histograma de condition vs model_year
+hist_button = st.button('Condición vs Año del Modelo')
+
+if hist_button:  # al hacer clic en el botón
+    st.write('Histograma que muestra la relación entre la condición del vehiculo y el año del modelo')
+    
+    # Crear el histograma
+    fig = px.histogram(
+        car_data,
+        x="model_year",
+        color="condition",
+        title="Histograma de Condición vs Año del Modelo",
+        barmode='overlay'  # Las barras se superponen
+    )
+    # Mostrar el gráfico Plotly interactivo 
+    st.plotly_chart(fig, use_container_width=True)
+
+
+price_condition_button = st.button('Precio vs Condición')
+
+if price_condition_button:  # al hacer clic en el botón
+    st.write('Grafica de dispersión de Precio vs Condición de los vehiculos')
+    
+    # Crear el gráfico de dispersión
+    fig_price_condition = px.scatter(
+        car_data,
+        x="price",          # Eje X
+        y="condition",      # Eje Y
+        color="condition",  # Color según la condición
+        title="Gráfico de Dispersión de Precio vs Condición",
+        labels={"price": "Precio ($)", "condition": "Condición"}
+    )
+    
+    # Mostrar el gráfico Plotly interactivo
+    st.plotly_chart(fig_price_condition, use_container_width=True)
